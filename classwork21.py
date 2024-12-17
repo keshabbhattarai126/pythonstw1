88c0ec337083273d4c4ed854b3113bc1a1422cca
keshabbhattarai126 <139044538+keshabbhattarai126@users.noreply.github.com>
2024-12-17 20:18:54 +0545

    first commit

---
+++ classwork21.py
@@ -0,0 +1,7 @@
+salary = float(input("Enter salary"))
+credit_score=float(input("Enter credit score"))
+if(salary>=50000 and credit_score>=700):
+    print("Eligible")
+else:
+    print("Not eligible")
+    
